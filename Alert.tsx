82edf5e3a3fbec13bc97a808b6fda3e82e8ea236
Steve <steveruizok@gmail.com>
2019-07-15 12:26:50 +0100

    Updates Alert

--- Alert.tsx
+++ Alert.tsx
@@ -1,57 +1,74 @@
-import { Alert as AntAlert, Icon } from "antd"
-import * as React from "react"
-import { PropertyControls, ControlType } from "framer"
-import "./App.css"
-import { ReactNode } from "react";
+import { Alert as AntAlert, Icon } from 'antd'
+import * as React from 'react'
+import { Frame, FrameProps, PropertyControls, ControlType } from 'framer'
+import './App.css'
+import { ReactNode } from 'react'
+import { centerInContainer } from './Utils'
 
-interface Props {
-    banner: boolean
-    closable: boolean
-    closeText: string | ReactNode
-    description: string | ReactNode
-    message: string | ReactNode
-    showIcon: boolean
-    type: string
-    onClose: () => {}
+interface Props extends FrameProps {
+	banner: boolean
+	closable: boolean
+	closeText: string | ReactNode
+	description: string | ReactNode
+	message: string | ReactNode
+	showIcon: boolean
+	type: 'success' | 'info' | 'warning' | 'error'
+	onClose: () => {}
 }
 
 export class Alert extends React.Component<Props> {
-    static defaultProps = {
-        width: 240,
-        height: 40,
-        message: "Text",
-        banner: false,
-        showIcon: true,
-        type: 'success'
-    }
+	static defaultProps = {
+		width: 240,
+		height: 40,
+		message: 'Text',
+		banner: false,
+		showIcon: true,
+		type: 'success',
+	}
 
-    static propertyControls: PropertyControls = {
-        message: { type: ControlType.String, title: "Message" },
-        banner: { type: ControlType.Boolean, title: "Banner" },
-        closable: { type: ControlType.Boolean, title: "Closable" },
-        closeText: { type: ControlType.String, title: "Close text" },
-        description: { type: ControlType.String, title: "Description" },
-        showIcon: { type: ControlType.Boolean, title: "Show Icon" },
-        type: {
-            type: ControlType.Enum,
-            title: "Type",
-            options: ["success", "info", "warning", "error"],
-        }
-    }
+	static propertyControls: PropertyControls = {
+		message: { type: ControlType.String, title: 'Message' },
+		banner: { type: ControlType.Boolean, title: 'Banner' },
+		closable: { type: ControlType.Boolean, title: 'Closable' },
+		closeText: { type: ControlType.String, title: 'Close text' },
+		description: { type: ControlType.String, title: 'Description' },
+		showIcon: { type: ControlType.Boolean, title: 'Show Icon' },
+		type: {
+			type: ControlType.Enum,
+			title: 'Type',
+			options: ['success', 'info', 'warning', 'error'],
+		},
+	}
 
-    render() {
-        const { height, width, banner,closable,closeText,description,showIcon, type, message } = this.props
-        return (
-            <AntAlert
-                style={{ height: `${height}px`, width: `${width}px` }}
-                banner={banner}
-                closable={closable}
-                closeText={closeText}
-                description={description}
-                showIcon={showIcon}
-                type={type}
-                message={message}
-            />
-        )
-    }
+	render() {
+		const {
+			banner,
+			closable,
+			closeText,
+			description,
+			showIcon,
+			type,
+			message,
+			...rest
+		} = this.props
+
+		const componentProps = {
+			banner,
+			closable,
+			closeText,
+			description,
+			showIcon,
+			type,
+			message,
+		}
+
+		return (
+			<Frame {...rest} {...centerInContainer(this.props)}>
+				<AntAlert
+					style={{ height: `100%`, width: `100%` }}
+					{...componentProps}
+				/>
+			</Frame>
+		)
+	}
 }
